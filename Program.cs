2e5bdd356d795b1d83367ff64975b503f6036546
BeeAri <122098016+BeeAri@users.noreply.github.com>
2023-04-06 17:12:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp_PI_PRIME_NUMBERS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("prime numbers having 3 digits and starting with 5");
+
+            bool primecontrol = false;
+            for (int i = 500; i < 600; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if ((i % j == 0)) { primecontrol = true; break; }
+                }
+                if (!primecontrol) Console.WriteLine(i);
+                primecontrol = false;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
